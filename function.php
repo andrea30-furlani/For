f21dba93a134221f2c26ef709511f3f3ed5a5014
ANDREA  CRISTINA REINO FURLANI <0220832221007@fatectq.local>
2023-09-27 10:52:06 -0300

    for

---
+++ function.php
@@ -0,0 +1,17 @@
+<?php include "cabecalho.php"; ?>
+        <h1>Function</h1>
+              
+        <div class="p">
+        <p>As funções são um dos blocos de construção fundamentais em JavaScript. 
+            Uma função em JavaScript é semelhante a um procedimento – um conjunto de instruções 
+            que executa uma tarefa ou calcula um valor, mas para que um procedimento seja 
+            qualificado como uma função, ele deve receber alguma entrada e retornar uma saída onde 
+            haja alguma relação óbvia entre o entrada e a saída. 
+            Para usar uma função, você deve defini-la em algum lugar do escopo a 
+            partir do qual deseja chamá-la.
+            </p>
+        </div>
+
+        
+ 
+<?php include "rodape.php"; ?>
---
+++ if.php
@@ -0,0 +1,18 @@
+<?php include "cabecalho.php"; ?>
+        <h1>Estrutura de decisão</h1>
+              
+        <div class="p">
+        <p>A condicional if é uma estrutura condicional que executa a afirmação, dentro do bloco, 
+se determinada condição for verdadeira. Se for falsa, executa as afirmações dentro de else.
+
+<h3>Sintaxe</h3>
+<p>if (condição) afirmação1 [else afirmação2]</p>
+
+<h3>Condição</h3>
+<p>Uma expressão (premissa) que pode ser avaliada como verdadeira (true) ou falsa (false), 
+veja lógica de programação para entender melhor.</p>
+        </p>
+        </div>
+        <?php include "rodape.php"; ?>
+        
+   
